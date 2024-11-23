c4e9a14a28709a1ccb80d8255789207daee75ad5
ADDFa <fratamaadha18@gmail.com>
2024-11-23 12:16:38 +0700

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import Test from "Pages/Test"
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <Test />
+    </StrictMode>
+)
